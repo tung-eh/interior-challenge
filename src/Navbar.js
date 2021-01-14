9cebf100b0d86d8bf4e596811ab5d912bdf25eac
Tung Teng <thetungctn@gmail.com>
2021-01-15 00:17:16 +0700

    Burger menu on mobile

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,3 +1,8 @@
+import React, { useState } from 'react'
+
+import Clear from './icons/Clear'
+import Menu from './icons/Menu'
+
 const Nav = ({ active, children }) => {
   return (
     <a href="/#" className={active ? 'font-bold' : ''}>
@@ -7,13 +12,39 @@ const Nav = ({ active, children }) => {
 }
 
 const Navbar = () => {
-  return (
-    <div className="grid grid-cols-4 gap-12">
+  const [open, setOpen] = useState(false)
+
+  const navs = (
+    <>
       <Nav active>Home</Nav>
       <Nav>Collection</Nav>
       <Nav>About</Nav>
       <Nav>Contact</Nav>
-    </div>
+    </>
+  )
+
+  return (
+    <>
+      <div className="hidden md:grid grid-cols-4 gap-12">{navs}</div>
+      <button className="md:hidden" onClick={() => setOpen(true)}>
+        <Menu />
+      </button>
+      <div
+        className={`fixed h-full w-0 top-0 right-0 flex flex-col justify-center overflow-hidden bg-gray-900 z-10 transition-all duration-500 ${
+          open ? 'w-full' : ''
+        }`}
+      >
+        <button
+          className="absolute top-4 right-4"
+          onClick={() => setOpen(false)}
+        >
+          <Clear />
+        </button>
+        <div className="grid gap-8 relative -top-10 text-lg justify-items-center px-14">
+          {navs}
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/icons/Menu.js
+++ src/icons/Menu.js
@@ -1,15 +1,15 @@
-const TrendingFlat = (props) => (
+const Menu = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    width="24"
     height="24"
+    viewBox="0 0 24 24"
+    width="24"
+    fill="currentColor"
     {...props}
   >
     <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M22 12l-4-4v3H3v2h15v3z" />
+    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
   </svg>
 )
 
-export default TrendingFlat
+export default Menu
